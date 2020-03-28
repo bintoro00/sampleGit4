2cf6643ce597bdec87889cec3055f84697524572
bintoro00 <58495311+bintoro00@users.noreply.github.com>
2020-03-28 12:58:15 +0900

    first commit.

---
+++ Assets/Script/Item.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Item : MonoBehaviour
+{
+	void OnTriggerEnter(Collider hit)
+	{
+		if(hit.CompareTag("Player")){
+
+			Destroy (gameObject);
+	}
+}
+}
